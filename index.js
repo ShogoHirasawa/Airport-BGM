def92558d5963bc3a360307240a41a43efa5550d
Shogo Hirasawa <origami.no.1@gmail.com>
2020-12-10 21:06:07 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function sound()
+{
+	// 対象となるID名
+	var id = 'sound-file' ;
+
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( id ).play() ;
+}
+
+function soundStop(){
+		// 対象となるID名
+		var id = 'sound-file' ;
+
+		// [ID:sound-file]の音声ファイルを再生[play()]する
+		document.getElementById( id ).pause()  ;
+	
+}
